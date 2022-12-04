1eb0b71e305ab0595e1fb0f38072e3337a0cf69b
hujuu <1961946+hujuu@users.noreply.github.com>
2022-12-04 12:58:45 +0900

    Get Started with FastAPI

---
+++ application/main.py
@@ -0,0 +1,20 @@
+"""main.py
+Python FastAPI Auth0 integration example
+"""
+
+from fastapi import FastAPI
+
+# Creates app instance
+app = FastAPI()
+
+
+@app.get("/api/public")
+def public():
+    """No access token required to access this route"""
+
+    result = {
+        "status": "success",
+        "msg": ("Hello from a public endpoint! You don't need to be "
+                "authenticated to see this.")
+    }
+    return result
